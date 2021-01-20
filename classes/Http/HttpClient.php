fc8f5f3f6b62d1779ef85a2519f0b9622a0a21d8
Archwill <archwill93@gmail.com>
2021-01-20 20:46:02 +0700

    Убрал тестовые данные для HTTP клиента

--- classes/Http/HttpClient.php
+++ classes/Http/HttpClient.php
@@ -8,8 +8,6 @@ class HttpClient
 {
     private $curl;
 
-    private $tempStorage = [];
-
     public function __construct()
     {
         $this->curl = curl_init();
@@ -17,42 +15,32 @@ class HttpClient
 
     public function get($url, $data)
     {
-//        curl_setopt($this->curl, CURLOPT_URL, $url . "?" . http_build_query($data));
-//        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-//        $result = curl_exec($this->curl);
-//        curl_close($this->curl);
-        $result = json_encode($this->tempStorage);
+        curl_setopt($this->curl, CURLOPT_URL, $url . "?" . http_build_query($data));
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($this->curl);
+        curl_close($this->curl);
         return $result;
     }
 
     public function post($url, $data)
     {
-//        curl_setopt($this->curl, CURLOPT_URL, $url);
-//        curl_setopt($this->curl, CURLOPT_POST, true);
-//        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
-//        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-//        $result = curl_exec($this->curl);
-//        curl_close($this->curl);
-            $id = count($this->tempStorage);
-            $this->tempStorage[$id]["id"] = $id;
-            $this->tempStorage[$id]["text"] = $data["text"];
-            $this->tempStorage[$id]["name"] = $data["name"];
-            $result = $this->tempStorage[$id];
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($this->curl);
+        curl_close($this->curl);
         return $result;
     }
 
     public function put($url, $data)
     {
-//        curl_setopt($this->curl, CURLOPT_URL, $url);
-//        curl_setopt($this->curl, CURLOPT_PUT, true);
-//        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($data));
-//        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-//        $result = curl_exec($this->curl);
-//        curl_close($this->curl);
-            $this->tempStorage[$data["ID"]]["text"] = $data["text"];
-            $this->tempStorage[$data["ID"]]["name"] = $data["name"];
-            $result = $this->tempStorage[$data["ID"]];
-
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_PUT, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($this->curl);
+        curl_close($this->curl);
         return $result;
     }
 }
