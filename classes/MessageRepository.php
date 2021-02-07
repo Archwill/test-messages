603d9e1a6c37c42228ee5228e25a6c031d7a288b
Konstantin Zharkov <archwill93@gmail.com>
2021-02-07 23:39:17 +0700

    Update MessageRepository.php

--- classes/MessageRepository.php
+++ classes/MessageRepository.php
@@ -5,32 +5,58 @@ namespace Classes;
 use Classes\Comment\Comment;
 use Classes\Comment\CommentFactory;
 use Classes\Http\HttpClient;
-use Classes\Services\MessageService;
 use Classes\MessageRepositoryInterface;
 
 class MessageRepository implements MessageRepositoryInterface
 {
-    private function initService(): MessageService
+    private $httpClient;
+    private $config;
+
+    public function __construct(HttpClientInterface $client)
     {
-        $httpClient = new HttpClient();
-        return new MessageService($httpClient);
+        $this->httpClient = $client;
+        $this->config = new Config();
     }
 
-    public function getAll(): array
+    private function validateComment(Comment $comment)
     {
-        $comments = json_decode($this->initService()->getAll(), true);
-        return array_map(array(CommentFactory::class, "createComment"), $comments);
+        foreach (get_object_vars($comment) as $field => $value) {
+            if ($field != "id" && empty($value)) {
+                throw new \Exception("Поле {$field} должно быть заполнено");
+            }
+        }
     }
 
-    public function getById($id): Comment
+    public function getById($id): string
     {
-        $comment = json_decode($this->initService()->getById($id), true);
+        $url = $this->config->getUrl("getById", $id);
+        $comment = $this->httpClient->get($url, []);
         return CommentFactory::createComment($comment);
     }
 
-    public function save(Comment $comment): Comment
+    public function getAll(): string
     {
-        $comment = json_decode($this->initService()->save($comment), true);
+        $url = $this->config->getUrl("getAll");
+        $comments = $this->httpClient->get($url, []);
+        return array_map(array(CommentFactory::class, "createComment"), $comments);
+    }
+
+    public function save(Comment $comment): string
+    {
+        $this->validateComment($comment);
+
+        $data = [
+            "name" => $comment->name,
+            "text" => $comment->text
+        ];
+        if (!is_null($comment->id) && $comment->id > 0) {
+            $url = $this->config->getUrl("update", $comment->id);
+            $comment = $this->httpClient->put($url, $data);
+        } else {
+            $url = $this->config->getUrl("add");
+            $comment = $this->httpClient->post($url, $data);
+        }
+        
         return CommentFactory::createComment($comment);
     }
 }
