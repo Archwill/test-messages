74cb7752f65b8b05b325a4c4f2cc53809a2851cc
Konstantin Zharkov <archwill93@gmail.com>
2021-02-07 23:16:29 +0700

    Update HttpClient.php

--- classes/Http/HttpClient.php
+++ classes/Http/HttpClient.php
@@ -8,47 +8,43 @@ class HttpClient implements HttpClientInterface
 {
     private $curl;
 
-    private function init()
+    private function executeQuery($curlOptions)
     {
         $this->curl = curl_init();
-    }
-
-    private function closeConnection()
-    {
-        curl_close($this->curl);
+        foreach ($curlOptions as $key => $value) {
+            curl_setopt($this->curl, $key, $value);
+        }
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($this->curl);
+        $this->closeConnection();
+        return $result;
     }
 
     public function get($url, $data): string
     {
-        $this->init();
-        curl_setopt($this->curl, CURLOPT_URL, $url . "?" . http_build_query($data));
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($this->curl);
-        $this->closeConnection();
-        return $result;
+        $options = [
+            CURLOPT_URL => $url . "?" . http_build_query($data),
+        ];
+        return $this->executeQuery($options);
     }
 
     public function post($url, $data): string
     {
-        $this->init();
-        curl_setopt($this->curl, CURLOPT_URL, $url);
-        curl_setopt($this->curl, CURLOPT_POST, true);
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($this->curl);
-        $this->closeConnection();
-        return $result;
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $data,
+        ];
+        return $this->executeQuery($options);
     }
 
     public function put($url, $data): string
     {
-        $this->init();
-        curl_setopt($this->curl, CURLOPT_URL, $url);
-        curl_setopt($this->curl, CURLOPT_PUT, true);
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($this->curl);
-        $this->closeConnection();
-        return $result;
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_PUT => true,
+            CURLOPT_POSTFIELDS => http_build_query($data),
+        ];
+        return $this->executeQuery($options);
     }
 }
